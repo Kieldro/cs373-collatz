fb3a789c35fff6f24cda6065d8a82b54846ae221
Ian Buitrago <kieldro@gmail.com>
2012-06-05 12:33:45 -0500

    updated readme with questions and added requirement issues.

--- README.txt
+++ README.txt
@@ -5,12 +5,15 @@ Last Name: Buitrago
 EID: ib
 CS Username: keo
 GitHub ID: Kieldro
-GitHub Repository Name:
+GitHub Repository Name: cs373-collatz
 Estimated number of hours: 10
 Actual    number of hours: 
 Comments:
-What does chmod ugo+x RunCollatz.py do?
-tab vs 2 space indention
+-What does chmod ugo+x RunCollatz.py do?
+-tab vs 2 space indention
+-ant vs bash script usage?
+- don't submit java files?
+- convention/style
 
 I attest that I have written every line of code that I have submitted
 and I take full responsibility for the origin of all the code submitted.
--- runTest.sh
+++ runTest.sh
@@ -1,24 +1,39 @@
 # file variables
-pattern="biblePattern.txt"
-source="bible.txt"
-outFile="results.txt"
-compile=true
-grep=true
-noError=true
+python=true
+java=false
+outFile="RunCollatz.out"
+compile=false
+grep=false
+noError=false
 
-if $compile; then
-	echo COMPILING...
-	javac strMatch.java
-	noError=([ $? == 0 ])
+if $python; then
+	echo TESTING PYTHON CODE...
+	python TestCollatz.py #> TestCollatz.py.out
+
+	echo RUNNING PYTHON CODE...
+	python RunCollatz.py < RunCollatz.in #> $outFile
 fi
 
-if $noError; then		# don't exectute if there were compilation errors
-	echo COMPILED successfully.
-	echo EXECTUTING...
-	java -ea strMatch $pattern $source $outFile
-fi
+if $java; then
+	if $compile; then
+		echo COMPILING...
+		javac *.java
+		noError=([ $? == 0 ])
+	fi
 
-if $grep; then
-	echo GREP for string in $source...
-	time grep -nm 1 'Can you find ThIs?' $source
+	if $noError; then		# don't exectute if there were compilation errors
+		echo COMPILED successfully.
+		echo EXECTUTING...
+		java -ea strMatch $pattern $source $outFile
+	fi
+
+	if $grep; then
+		echo GREP for string in $source...
+		time grep -nm 1 'Can you find ThIs?' $source
+	fi
 fi
+<< '--MULTICOMMENT--'
+
+free 
+comments!
+--MULTICOMMENT--
